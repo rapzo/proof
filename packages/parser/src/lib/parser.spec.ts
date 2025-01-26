f251f8f2866ca0f024706e49350f001679650d5b
Rui Lima <ruipedro.lima@gmail.com>
2025-01-26 09:50:13 +0000

    Add devcontainer config for codespaces

--- packages/parser/src/lib/parser.spec.ts
+++ packages/parser/src/lib/parser.spec.ts
@@ -2,6 +2,6 @@ import { parser } from './parser';
 
 describe('parser', () => {
   it('should work', () => {
-    expect(parser()).toEqual('parser');
+    expect(parser('x')).toEqual('parser');
   });
 });
--- packages/parser/src/lib/parser.ts
+++ packages/parser/src/lib/parser.ts
@@ -1,3 +1,23 @@
-export function parser(): string {
+import fs from 'node:fs';
+import { resolve } from 'node:path';
+import { finished } from 'node:stream/promises';
+import { parse, transform, stringify, generate } from 'csv';
+
+export async function parser(filepath: string): Promise<string> {
+  await finished(
+    generate({ length: 10 }).pipe(
+      fs.createWriteStream(resolve(__dirname, 'example.fs.input.csv'))
+    )
+  );
+
+  await finished(
+    fs
+      .createReadStream(`${__dirname}/example.fs.input.csv`)
+      .pipe(parse())
+      .pipe(transform((record) => record.reverse()))
+      .pipe(stringify())
+      .pipe(fs.createWriteStream(`${__dirname}/example.fs.output.csv`))
+  );
+
   return 'parser';
 }
